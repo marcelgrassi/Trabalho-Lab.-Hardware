3bc70961680591962695ab5193fd1546c4cdde38
Marcel Grassi <marcelgrassi@gmail.com>
2010-05-31 23:54:21 -0400

    Soma 1 no complemento de 2

--- ALU.vhd
+++ ALU.vhd
@@ -5,7 +5,7 @@ entity ALU is
 	port( a, b : in bit_vector(7 downto 0);
 		  aluop : in bit_vector(1 downto 0);
 		  result : out bit_vector(7 downto 0);
-		  zero : inout bit
+		  zero : out bit
 		);
 end entity;
 
@@ -17,6 +17,12 @@ begin
 		variable ind : integer;
 	begin
 		
+		if(a = b)
+			zero <= '1';
+		else
+			zero <= '0';
+		end if;		
+			
 		-- aluop = "00" = +adição
 		case aluop is
 			when "00" =>	
@@ -25,7 +31,8 @@ begin
 					if((a(ind) xor b(ind)) = '1')then
 						result(ind) <= a(ind) xor b(ind);
 					elsif(a(ind) = '0')then
-							result(ind) <= a(ind) and b(ind);
+							result(ind) <= ((a(ind) and b(ind)) xor carry);
+							carry := '0';
 					else
 							result(ind) <= ((a(ind) xor b(ind)) or carry);
 							carry := '1';
@@ -34,17 +41,35 @@ begin
 			
 			-- aluop = "01" = -subtração
 			when "01" =>
-				-- laço para o complemento de 2
 				carry <= '0';
+				-- laço para o complemento de 2
 				for	ind in 0 to 7 loop 
 					result(ind) <= not b(ind); 
 				end loop;
+				
+				carry := '1';
+				for ind in 0 to 7 loop
+					if((result(ind) xor 'carry') = '1')
+						result(ind) <= (result(ind) or carry);
+						carry := '0';
+					elsif(result(ind) = '0')	
+						result(ind) <= (result(ind) and carry);
+						carry := '0';
+					else
+						result(ind) <= (result(ind) xor carry);
+						carry := '1';
+					end if;
+				end loop;			
+						
+				
 				-- calculo para a subtração
+				carry := '0';
 				for ind in 0 to 7 loop
 					if((a(ind) xor result(ind)) = '1')then
 						result(ind) <= a(ind) xor result(ind);
 					elsif(a(ind) = '0')then
-							result(ind) <= a(ind) and result(ind);
+							result(ind) <= ((a(ind) and result(ind)) xor carry);
+							carry := '0';
 					else
 							result(ind) <= ((a(ind) xor result(ind)) or carry);
 							carry := '1';
