87380874dce0480d723ff83ef61fc1b7a973ac14
Marcel Grassi <marcelgrassi@gmail.com>
2010-06-01 02:01:59 -0400

    Gerador de clock

---
+++ clock/clock_generator.vhd
@@ -0,0 +1,24 @@
+entity clock_generator is
+port(	Halt : in bit;
+		Clk : out bit
+	);
+end entity;
+
+architecture arq_clock of clock_generator is
+begin
+
+	process(Clk)
+	begin
+		
+		Clk <= not Clk after 1 ns;
+		
+		if(Halt = '1')
+			Clk <= not Clk wait 1 ns;
+			
+		end if;	
+		
+	
+	end process;
+
+
+end architecture;
